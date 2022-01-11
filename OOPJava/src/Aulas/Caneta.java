caafb05fa289fcbb3640ff6e79ffe593854b2f37
Leonardo Arruda <leonardo.earruda@gmail.com>
2022-01-10 21:00:45 -0300

    First commit

---
+++ OOPJava/src/Aulas/Caneta.java
@@ -0,0 +1,38 @@
+package Aulas;
+
+public class Caneta {
+
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+
+    public void status() {
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Esta tampada? " + this.tampada);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+    }
+
+    public void rabiscar() {
+        if (this.tampada) {
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando...");
+        }
+    }
+
+    public void tampar() {
+        this.tampada = true;
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
